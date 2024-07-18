3c1ba284fb86e9b7f808944f2baef46475e1c32e
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-07-18 15:36:48 +0500

    finished

---
+++ script.js
@@ -0,0 +1,37 @@
+Tone.start()
+const synth = new Tone.Synth().toDestination()
+
+
+function playnote(note) {
+    synth.triggerAttackRelease(`${note}4`, '8n')
+    document.getElementById(note).style.background = '#F6683B'
+    setTimeout(() => {
+        document.getElementById(note).style.background = 'white'
+    }, 200)
+}
+
+document.onkeydown = function (e) {
+    e = e || window.event
+    var key = e.which || e.keyCode
+    if (key === 83) {
+        playnote('C')
+    }
+    else if (key === 68) {
+        playnote('D')
+    }
+    else if (key === 70) {
+        playnote('E')
+    }
+    else if (key === 71) {
+        playnote('F')
+    }
+    else if (key === 72) {
+        playnote('G')
+    }
+    else if (key === 74) {
+        playnote('A')
+    }
+    else if (key === 75) {
+        playnote('B')
+    }
+}
